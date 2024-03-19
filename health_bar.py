1bbe668a6ff7088534c2161205bae20a77ef7f9e
mstepan-vendavo <stepanmichal82@gmail.com>
2024-03-19 14:13:58 +0100

    init

--- health_bar.py
+++ health_bar.py
@@ -1,5 +1,6 @@
 # ------------ imports ------------
 import os
+import sys
 
 # ------------ setup ------------
 os.system("")
@@ -10,24 +11,23 @@ class HealthBar:
     symbol_remaining: str = "█"
     symbol_lost: str = "_"
     barrier: str = "|"
-    colors: dict = {"red": "\033[91m",
-                    "purple": "\33[95m",
-                    "blue": "\33[34m",
-                    "blue2": "\33[36m",
-                    "blue3": "\33[96m",
-                    "green": "\033[92m",
-                    "green2": "\033[32m",
-                    "brown": "\33[33m",
-                    "yellow": "\33[93m",
-                    "grey": "\33[37m",
-                    "default": "\033[0m"
-                    }
+    colors: dict = {
+        "red": "\033[91m",
+        "purple": "\33[95m",
+        "blue": "\33[34m",
+        "blue2": "\33[36m",
+        "blue3": "\33[96m",
+        "green": "\033[92m",
+        "green2": "\033[32m",
+        "brown": "\33[33m",
+        "yellow": "\33[93m",
+        "grey": "\33[37m",
+        "default": "\033[0m",
+    }
 
-    def __init__(self,
-                 entity,
-                 length: int = 20,
-                 is_colored: bool = True,
-                 color: str = "") -> None:
+    def __init__(
+        self, entity, length: int = 20, is_colored: bool = True, color: str = ""
+    ) -> None:
         self.entity = entity
         self.length = length
         self.max_value = entity.health_max
@@ -42,10 +42,17 @@ class HealthBar:
     def draw(self) -> None:
         remaining_bars = round(self.current_value / self.max_value * self.length)
         lost_bars = self.length - remaining_bars
-        print(f"{self.entity.name}'s HEALTH: {self.entity.health}/{self.entity.health_max}")
-        print(f"{self.barrier}"
-              f"{self.color if self.is_colored else ''}"
-              f"{remaining_bars * self.symbol_remaining}"
-              f"{lost_bars * self.symbol_lost}"
-              f"{self.colors['default'] if self.is_colored else ''}"
-              f"{self.barrier}")
+        print(
+            f"{self.entity.name}'s HEALTH: {self.entity.health}/{self.entity.health_max}"
+        )
+        try:
+            print(
+                f"{self.barrier}"
+                f"{self.color if self.is_colored else ''}"
+                f"{remaining_bars * self.symbol_remaining}"
+                f"{lost_bars * self.symbol_lost}"
+                f"{self.colors['default'] if self.is_colored else ''}"
+                f"{self.barrier}".encode("utf-8").decode(sys.stdout.encoding)
+            )
+        except UnicodeEncodeError:
+            print("Cannot display colored output due to encoding limitations.")
--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from character import Hero, Enemy
 from weapon import short_bow, iron_sword
 
 # ------------ setup ------------
+game_loop = True
 hero = Hero(name="Hero", health=100)
 hero.equip(iron_sword)
 enemy = Enemy(name="Enemy", health=100, weapon=short_bow)
 
 # ------------ game loop ------------
-while True:
+while game_loop:
     os.system("cls")
 
     hero.attack(enemy)
@@ -18,4 +19,8 @@ while True:
     hero.health_bar.draw()
     enemy.health_bar.draw()
 
-    input()
+    if enemy.health == 0 or hero.health == 0:
+        print("End")
+        game_loop = False
+
+    # input()
